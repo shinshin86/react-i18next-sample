c7111876d3fc2e6d13f93f259176fd892be7cdf8
shinshin86 <beagles1986@gmail.com>
2019-09-29 19:00:05 +0900

    Add sample of using "Trans"

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './i18n';
-import { withTranslation } from 'react-i18next';
+import { withTranslation, Trans } from 'react-i18next';
 
 function changeLanguage(i18n, lang) {
   i18n.changeLanguage(lang);
 }
 
 function App({ t, i18n }) {
+  const filename = 'src/App.js';
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Trans i18nKey="Save to Reload text">
+            Edit <code>{{ filename }}</code> and save to reload.
+          </Trans>
         </p>
         <a
           className="App-link"
