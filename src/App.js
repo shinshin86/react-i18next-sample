0bf809a70638c9a63fce8823939efdb948331c6d
shinshin86 <beagles1986@gmail.com>
2023-04-25 19:54:18 +0900

    Fix constant

--- src/App.js
+++ src/App.js
@@ -4,21 +4,21 @@ import './App.css';
 import './i18n';
 import { withTranslation, Trans } from 'react-i18next';
 
+const FILE_NAME = 'src/App.js';
+
 const langTextMap = {
   en: 'Language',
   ja: '言語',
 };
 
 function App({ t, i18n }) {
-  const filename = 'src/App.js';
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <Trans i18nKey="Save to Reload text">
-            Edit <code>{{ filename }}</code> and save to reload.
+            Edit <code>{{ filename: FILE_NAME }}</code> and save to reload.
           </Trans>
         </p>
         <a
